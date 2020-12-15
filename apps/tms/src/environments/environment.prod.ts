e135fe4b6114f35792ff65046384c6294e567205
Fernando Veloz <fernandovelozcleto@gmail.com>
2020-12-14 22:09:44 -0600

    endpoint production

--- apps/tms/src/environments/environment.prod.ts
+++ apps/tms/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isMockEnabled: false, // You have to switch this, when your real back-end is done
-  endpoint: '/',
+  endpoint: 'http://tms-env.eba-hkiaey36.us-west-1.elasticbeanstalk.com/',
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
   mapboxKey: 'pk.eyJ1Ijoiam9uaHlndXkzIiwiYSI6ImNrZXU4aGVjeTJvczYyc3FtMzludjVwcG8ifQ.DC_kqyuEe4ahC18G22qXPw',
 };
