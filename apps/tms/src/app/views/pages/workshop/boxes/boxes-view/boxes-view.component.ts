238852c555631931088ce43c842e906307ea837d
Fernando Veloz <fernandovelozcleto@gmail.com>
2021-04-18 22:13:02 -0500

    short path

--- apps/tms/src/app/views/pages/workshop/boxes/boxes-view/boxes-view.component.ts
+++ apps/tms/src/app/views/pages/workshop/boxes/boxes-view/boxes-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { BoxModel } from '../../../../../core/models';
+import { BoxModel } from '@tms/models';
 
 @Component({
   selector: 'b404-boxes-view',
